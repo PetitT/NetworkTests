2269f77a82e2ee69ae94cf511b3955ea4926aba9
Thibaut <xcthib@hotmail.com>
2022-09-13 07:26:07 +0200

    readd usafu

---
+++ NetworkTest/Assets/Scripts/USAFU/Runtime/ExternalUI/ExternalUI.cs
@@ -0,0 +1,18 @@
+﻿using FishingCactus.User;
+using System.Threading.Tasks;
+
+namespace FishingCactus.ExternalUI
+{
+    public class ExternalUI : ExternalUIBase
+    {
+        public override Task<ShowLoginUiResult> ShowLoginUi( int controller_id, bool show_skip_button )
+        {
+            return Task.FromResult( new ShowLoginUiResult
+            {
+                Success = true,
+                UserId = new UniqueUserId(),
+                ControllerId = controller_id 
+            });
+        }
+    }
+}
