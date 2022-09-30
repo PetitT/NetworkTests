c656a0dda5978e8915e3c2c26b00943aad617eda
PetitT <xcthib@hotmail.com>
2022-09-30 17:49:48 +0200

    Update onlineleaderboard

--- NetworkTest/Assets/Usafu/Runtime/GenericPlatform/Leaderboards/OnlineLeaderboards.cs
+++ NetworkTest/Assets/Usafu/Runtime/GenericPlatform/Leaderboards/OnlineLeaderboards.cs
@@ -3,15 +3,5 @@ using FishingCactus.User;
 
 namespace FishingCactus.OnlineLeaderboards
 {
-    public class LeaderboardEntry : ILeaderboardEntry
-    {
-        public IUniqueUserId UserID { get; set; }
-        public string UserName { get; set; }
-        public int Score { get; set; }
-        public LeaderboardEntry() { }
-    }
-
-    public class OnlineLeaderboard : OnlineLeaderboardsBase { 
-        public override void Initialize( Settings platform_settings ) {  } 
-    }
+    public class OnlineLeaderboard : OnlineLeaderboardsBase { }
 }
